2b89ed018dff495a41bd306b4dc7a5ae3935b203
Tanvir Ahmed <tanvir.ahmed@metafour.com>
2017-01-23 08:24:31 +0600

    Added JSP ViewResolver

---
+++ README.md
@@ -0,0 +1,16 @@
+# spring-starter-logback
+
+* http://localhost:8080/
+
+* http://docs.spring.io/spring/docs/current/javadoc-api/org/springframework/web/WebApplicationInitializer.html
+* https://kielczewski.eu/2013/11/spring-mvc-without-web-xml-using-webapplicationinitializer/
+* https://dzone.com/articles/spring-1
+* https://www.mkyong.com/spring-mvc/spring-mvc-logback-slf4j-example/
+* https://www.eclipse.org/jetty/documentation/9.4.x/jetty-maven-plugin.html
+
+* https://github.com/bkielczewski/example-spring-mvc-initializer/
+* https://github.com/kgleong/spring-mvc-template
+
+* https://github.com/qos-ch/logback-extensions/wiki/Spring
+* https://github.com/tanvir-ahmed-m4/demo-elk-log
+
--- src/main/java/com/example/WebMvcConfig.java
+++ src/main/java/com/example/WebMvcConfig.java
@@ -1,12 +1,33 @@
 package com.example;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-@EnableWebMvc
 @Configuration
+@EnableWebMvc
 @ComponentScan(basePackages = "com.example")
 class WebMvcConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    /**
+     * Provide a view resolver to map views to the correct template files.
+     *
+     * @return
+     */
+    @Bean
+    public ViewResolver getViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/pages/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
 }
